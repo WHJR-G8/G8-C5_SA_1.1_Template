ca8b68fd53e4f12c6288af0872e583b0e8a357af
WHJR-G8 <84369631+WHJR-G8@users.noreply.github.com>
2021-10-13 20:07:49 +0530

    Create Template_SA_1.1.py

---
+++ Template_SA_1.1.py
@@ -0,0 +1,20 @@
+import turtle
+
+#write the code for if block over here
+
+else:
+    
+        for x in range(20):
+            turtle.fillcolor("red")
+            turtle.begin_fill()
+            turtle.forward(side)
+            if x % 2 == 0:
+                turtle.left(175)
+                turtle.pencolor('blue')
+            else:
+                turtle.left(225)
+                turtle.pencolor('yellow')
+
+        turtle.end_fill()    
+
+turtle.ht()
