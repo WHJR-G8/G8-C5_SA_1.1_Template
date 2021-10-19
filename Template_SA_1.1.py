376e23f1ee39bebaaba05210bcd356c627a3cf1f
WHJR-G8 <84369631+WHJR-G8@users.noreply.github.com>
2021-10-19 17:03:08 +0530

    Update Template_SA_1.1.py

--- Template_SA_1.1.py
+++ Template_SA_1.1.py
@@ -5,6 +5,8 @@ turtle.shape('turtle')
 
 side = int(input("Enter the side value"))
 
+#For Student 1
+
 #write the code for if block over here
 
     for x in range(20):
@@ -19,9 +21,21 @@ side = int(input("Enter the side value"))
             turtle.pencolor("")#Make the pencolor as "green"
 
     #End the color fill
-    
+
+#For Student 2
+
 #write the code for elif block over here
 
+    for x in range(20):
+        turtle.fillcolor("")#Fill the color as "cyan"
+        #Start the color filling
+        turtle.forward(side)
+        #write the code for if block over here
+            turtle.left(175)
+            turtle.pencolor("")#Make the pencolor as "yellow"
+        else:
+            turtle.left(225)
+            turtle.pencolor("")#Make the pencolor as "blue"
 
     #End the color fill
 
