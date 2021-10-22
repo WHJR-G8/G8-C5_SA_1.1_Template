b6cba802f50daa4298c5fce8db7e2104022af42f
WHJR-G8 <84369631+WHJR-G8@users.noreply.github.com>
2021-10-22 10:44:08 +0530

    Update Template_SA_1.1.py

--- Template_SA_1.1.py
+++ Template_SA_1.1.py
@@ -7,39 +7,37 @@ side = int(input("Enter the side value"))
 
 #For Student 1
 
-#Hint 1: Write an "if" condition to obtain side value greater than 0 and less than or equal to 100.
 
     for x in range(20):
-        turtle.fillcolor("")#Hint 2: Fill the color as "yellow".
+        turtle.fillcolor("")
         turtle.begin_fill()
         turtle.forward(side)
         
-        #Hint 3: Write an "if" condition, "if x%2==0", inside the "for" loop.
+       
             turtle.left(175)
-            turtle.pencolor("")#Hint 4: Make the pencolor as "red"
+            turtle.pencolor("")
             
-        #Hint 5: Write an "else" condition.
+        
             turtle.left(225)
-            turtle.pencolor("")#Hint 6: Make the pencolor as "green".
-
+            turtle.pencolor("")
+            
     turtle.end_fill()
 
 #For Student 2
 
-#Hint 1: Write an "elif" condition to obtain side values greater than 100 and less than equal to 200.
 
     for x in range(20):
-        turtle.fillcolor("") #Hint 2: Fill the color as "cyan"
+        turtle.fillcolor("")
         turtle.begin_fill()
         turtle.forward(side)
         
-        #Hint 3: Write an "if" condition, "if x%2==0".
+       
             turtle.left(175)
-            turtle.pencolor("") #Hint 4: Make the pencolor as "yellow"
+            turtle.pencolor("")
             
-        #Hint 5: Write an "else" condition.
+        
             turtle.left(225)
-            turtle.pencolor("") #Hint 6: Make the pencolor as "blue"
+            turtle.pencolor("")
 
     turtle.end_fill()
 
